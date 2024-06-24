6a68bc14bb6c1d961063a8894c91c386aa23d305
Yufeng Liu <yufeng.liu15@gmail.com>
2024-06-24 13:47:38 -0400

    slowly going through html elements

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # PapersWithKindle
-An automatic service that sends research papers to your kindle
+An automatic service that sends research papers to your kindle. I want to peek my head into the world of research, so might as well go deep. There are 2 reasons why I developed this project:
+1. Learn AWS and Docker
+2. Force myself to read papers every couple of days
 
 # Activate Virtual Environment
 python3 -m venv /Users/yufeng/PapersWithKindle
@@ -8,3 +10,10 @@ source /Users/yufeng/PapersWithKindle/bin/activate
 # Resources
 All papers have been scrapped from this website, which is annually maintained by Jeff Huang.
 https://jeffhuang.com/best_paper_awards/
+
+## Pros 
+- Clear and organized format for displaying all the research papers. 
+- Every paper here is distinguished
+
+## Cons
+- Not real time 
--- scraper.py
+++ scraper.py
@@ -2,7 +2,49 @@
 # this is because it sorts it by category for me :D
 
 from bs4 import BeautifulSoup
+import requests
+import re
 
-soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
-print(soup.prettify())
+URL = "https://jeffhuang.com/best_paper_awards/conferences.html"
+
+res = requests.get(URL)
+htmlData = res.content
+parsedData = BeautifulSoup(htmlData, "html.parser")
+tables = parsedData.find_all('table')
+
+# regex
+# Regular expression pattern
+pattern = r'\((.*?)\)'
+
+
+# Structure:
+# { "Category 1": {
+#      "Year": value,
+#      "Title": value,
+#      "Authors": value,
+#      "Link": value
+#   },
+#   "Category 2": {
+#       .... 
+#   }
+# }
+count = 0
+for table in tables:
+    category = re.findall(pattern, table.find('th').string)[0]
+    rows = table.find('tbody').find_all('tr')
+    for row in rows:
+        # extract Year
+        #if (row.find('th') is None):
+        #    print(lastYear)
+        #else:
+        #    year = row.find('th').find('a').string
+        #    lastYear = year
+        #    print(year)
+        
+        # extract Title
+        count += 1
+        
+        
+print(count)
+
 
