d1687e9dc15e86eb79d799b1cbd1133acc9c851c
Yufeng Liu <yufeng.liu15@gmail.com>
2024-06-25 21:11:10 -0400

    parsing script done

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import json
 
 URL = "https://jeffhuang.com/best_paper_awards/conferences.html"
 
@@ -16,7 +17,7 @@ tables = parsedData.find_all('table')
 # Regular expression pattern
 pattern = r'\((.*?)\)'
 
-
+papers = {}
 # Structure:
 # { "Category 1": {
 #      "Year": value,
@@ -31,26 +32,52 @@ pattern = r'\((.*?)\)'
 for table in tables:
     category = re.findall(pattern, table.find('th').string)[0]
     rows = table.find('tbody').find_all('tr')
+    
+    papers = {category: None}
+    papersOfCategory = []
     for row in rows:
-        # extract Year
-        if (row.find('th') is None):
-            year = lastYear
-        else:
-            year = row.find('th').find('a').string
-            lastYear = year
+        try: 
+            # extract Year
+            if (row.find('th') is None):
+                year = lastYear
+            else:
+                year = row.find('th').find('a').string
+                lastYear = year
+        except:
+            print("Couldn't parse for Year!")
+        
+        try:
+            # extract Title
+            title = row.find('td', class_="paper-title").string
+        except:
+            print("Couldn't parse for Title!")
+        
+        try:
+            # extract Link
+            link = row.find('td', class_="paper-title").find('a').attrs['href']
+        except:
+            print("Couldn't parse for Link!")
+        
+        try:
+            # extract Authors
+            author = row.find('td', class_="authors").contents[0]
+        except:
+            print("Couldn't parse for Author!")
             
-        # extract Title
-        title = row.find('td', class_="paper-title").string
-        
-        # extract Link
-        link = row.find('td', class_="paper-title").find('a').attrs['href']
-        
-        # extract Authors
-        author = row.find('td', class_="authors").contents[0]
-        print(author)
-        
-    break
-        
-        
+        # add all parsed fields into an array
+        paper = [year, title, author, link]
+        papersOfCategory.append(paper)
+    
+    # send to dictionary
+    papers[category] = papersOfCategory
+
+# export as JSON
+try:  
+    with open("papers.json", "w") as outfile:
+        json.dump(papers,outfile)
+    print("Successfully Exported to JSON")
+except:
+    print("Unable to Export to JSON")
+
 
 
