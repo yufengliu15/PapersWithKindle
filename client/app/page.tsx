1b925b7e76034ef03062d35c159b359c73a76aaa
Yufeng Liu <yufeng.liu15@gmail.com>
2024-07-14 09:30:35 -0400

    basic intro page done

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,35 @@
+import "../styles/global.css"
+
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
+    return (
+        <div className="content">
+            <div className="header">
+                <h1>paperswithkindle</h1>
+                <h1><a href="https://yufengliu.tech" target="_blank">yufengliu.tech</a></h1>
+            </div>
+            <div className="ascii-art">
+                <p>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;_.--._  _.--._ <br></br>
+            ,-=.-":;:;:;\':;:;:;"-._ <br></br>
+            \\\:;:;:;:;:;\:;:;:;:;:;\  <br></br>
+            &nbsp;\\\:;:;:;:;:;\:;:;:;:;:;\   <br></br>
+            &nbsp;&nbsp;\\\:;:;:;:;:;\:;:;:;:;:;\    <br></br>
+            &nbsp;&nbsp;&nbsp;\\\:;:;:;:;:;\:;::;:;:;:\     <br></br>
+            &nbsp;&nbsp;&nbsp;&nbsp;\\\;:;::;:;:;\:;:;:;::;:\    <br></br>  
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\\;;:;:_:--:\:_:--:_;:;\    <br></br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\\\_.-"&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"-._\<br></br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;\`_..--""--.;.--""--.._.|    <br></br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;"      
+                </p>
+            </div>
+            <div className="email-signup">
+                <div className="email-signup-text">
+                    <p>
+                    join the waitlist now
+                    </p>
+                </div>
+                
+            </div>
+        </div>
+    )
 }
